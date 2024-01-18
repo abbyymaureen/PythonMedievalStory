dc0da4d6e2626216589ed4422c53b8d7dd27ee5a
Abby Brown <amb0330@icloud.com>
2024-01-18 10:23:09 -0700

    Create main file

---
+++ PythonMedievalStory/main.py
@@ -0,0 +1,11 @@
+"""
+@author abby_brown
+@filename main.py
+@date 01/18/24
+
+This program allows the user to play a console-based medieval times game. The user will be able
+to travel around the medieval world, kill magical creatures, and make friends along the way.
+"""
+
+if __name__ == '__main__':
+    pass
