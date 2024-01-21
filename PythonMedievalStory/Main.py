85e56e5dbc90c38afee2dd58bae285f90133263c
Abby Brown <amb0330@icloud.com>
2024-01-21 15:35:21 -0700

    Finish fight function

--- PythonMedievalStory/Main.py
+++ PythonMedievalStory/Main.py
@@ -9,6 +9,7 @@ to travel around the medieval world, kill magical creatures, and make friends al
 import Location
 import Entity
 import random
+import time
 
 
 def setup():
@@ -46,26 +47,107 @@ def setup():
     return places, people, monsters
 
 
+def menu():
+    print("*** Medieval Story World ***\n1. Go to Town\n2. Go to Market\n3. Go to Cave")
+    try:
+        menu_choice = int(input("> "))
+    except ValueError:
+        # Handle the case when the user enters a non-integer value
+        print("Please enter a valid integer.")
+
+
 def get_main_character(people):
     for person in people:
         if person.name == "Odilie Fuchsg":
             return person
 
 
-def fight(places, people, monsters):
+def fight(people, monsters):
     # get the main character
     me = get_main_character(people)
 
-    # get the random monster to fight
-    rand_index = random.randint(0, len(monsters) - 1)
-    monster = monsters[rand_index]
+    if me.getHealth() <= 0:
+        print("You don't have any health! You have died. Goodbye!")
+    else:
+        # get the random monster to fight
+        rand_index = random.randint(0, len(monsters) - 1)
+        monster = monsters[rand_index]
 
+        if not monster.getGood():
+            print(f"Fighting {monster.getName()}, a {monster.getSpecies()}!")
+            while me.getHealth() >= 0 or monster.getHealth >= 0:
+                print(f"You have {me.getHealth()} health and {monster.getName()} has {monster.getHealth()} health.")
+                monster = monster.setHealth(monster.getHealth() - me.getStrength())
+                time.sleep(2)
+                print(f"{monster.getName()} now has a health of {monster.getHealth()} and you have a health of {me.getHealth()}.")
+                me = me.setHealth(me.getHealth() - monster.getStrength())
+                time.sleep(2)
+                print(f"The monster attacked! You now have a health of {me.getHealth()} and {monster.getName()} has a health of {monster.getHealth()}.")
 
+            if me.getHealth <= 0:
+                print(f"You died fighting the {monster.getName()} Goodbye!")
+            elif monster.getHealth <= 0:
+                print(f"You defeated {monster.getName()}! You have now won {monster.getGold} gold.")
+                me.setGold(me.getGold + monster.getGold)
+                monsters.pop(monster.getIndex())
+                menu()
+            else:
+                print(f"You and {monster.getName()} both died... You have now lost. Goodbye!")
+        else:
+            print(f"You have stumbled upon a magical {monster.getSpecies()}. These creatures are good!")
+            print(f"Fairy: My name is {monster.getName()}. It is so wonderful to meet you, {me.getName()}!")
+            print(f"If you are able to answer this challenging riddle, I will give you 25 gold!\nReady?")
+            time.sleep(2)
+            print("What can you break, even if you never pick it up or touch it? ")
+            answer_one = input("> ").lower()
+            if answer_one == "a promise" or answer_one == "promise" or answer_one == "promises":
+                print("You did it! A promise can be broken without being touched! You have won 25 gold! However,"
+                      "would you like a chance to double your gold...? If you get this next riddle right, you win"
+                      "50 gold instead! But if you answer incorrectly, you win nothing.")
+                play_again = input("So, what do you say (y/n): ").lower()
+                if play_again == "yes" or play_again == "y":
+                    print("Wonderful choice! Get ready for the most challenging question of your life...")
+                    time.sleep(2)
+                    print("I have branches, but no fruit, trunk or leaves. What am I?")
+                    answer_two = input("> ").lower()
+                    if answer_two == "bank" or answer_two == "banks" or answer_two == "a bank":
+                        print("Congratulations! Banks was the right answer! Here is your 50 gold as promised...")
+                        time.sleep(2)
+                        me.setGold(me.getGold() + 50)
+                        print(f"You now have {me.getGold()} gold.\n")
+                        monsters.pop(monster.getIndex())
+                        menu()
+                    else:
+                        print("I'm sorry, the correct answer was 'bank.' Better luck next time. As a result of losing"
+                              "this question, you don't win any gold...\n")
+                        monsters.pop(monster.getIndex())
+                        menu()
+                elif play_again == "no" or "n":
+                    print("Playing it safe I see, wise of you. Here's your 25 gold as promised...")
+                    time.sleep(2)
+                    me.setGold(me.getGold() + 25)
+                    print(f"You now have {me.getGold()} gold.")
+                    monsters.pop(monster.getIndex())
+                    menu()
+                else:
+                    print("That wasn't a valid response. You just lost all of your gold!")
+                    me.setGold(0)
+                    print(f"You now have {me.getGold()} gold.")
+                    monsters.pop(monster.getIndex())
+                    menu()
+            else:
+                print("I'm sorry, the correct answer was 'a promise.' You don't win this time, but here's an extra"
+                      "5 gold just for playing along :)")
+                time.sleep(2)
+                me.setGold(me.getGold() + 5)
+                print(f"You now have {me.getGold()} gold.")
+                monsters.pop(monster.getIndex())
+                menu()
 
 
 def main():
     places, people, monsters = setup()
-    fight(places, people, monsters)
+    fight(people, monsters)
 
 
 if __name__ == '__main__':
